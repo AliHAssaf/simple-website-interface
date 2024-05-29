118fe8d25d705b1d3542affa4253bae290ab8455
Abo-alzein <aliassaf0875758474757@gmail.com>
2024-05-29 16:05:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var dashImg = document.querySelector(".dashImg");
+document.querySelector(".storagebtn").addEventListener("click",() =>{
+    dashImg.src = "img/dashboard1.jpeg"
+})
+document.querySelector(".interfacebtn").addEventListener("click",() =>{
+    dashImg.src = "img/dashboard2.png"
+})
+document.querySelector(".throughputbtn").addEventListener("click",() =>{
+    dashImg.src = "img/dashboard3.png"
+})
